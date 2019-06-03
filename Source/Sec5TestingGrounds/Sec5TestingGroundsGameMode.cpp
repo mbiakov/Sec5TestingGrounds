3936faed7e22609ecc730bfd02e5e72e1bc474de
Mickael Biakov <mickael.biakov@gmail.com>
2019-06-03 16:18:50 +0200

    Use UE4 Style Guide for naming conventions

--- Source/Sec5TestingGrounds/Sec5TestingGroundsGameMode.cpp
+++ Source/Sec5TestingGrounds/Sec5TestingGroundsGameMode.cpp
@@ -9,7 +9,7 @@ ASec5TestingGroundsGameMode::ASec5TestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Characters/CharacterUnited"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Characters/BP_Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
