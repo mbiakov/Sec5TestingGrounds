b80bff39baf1b524e8eeaa56159a1c27779ba4f5
Mickael Biakov <mickael.biakov@gmail.com>
2019-06-06 14:59:16 +0200

    Add verification for empty location to spawn in AGround::PlaceActors()

--- Source/Sec5TestingGrounds/Terrain/Ground.cpp
+++ Source/Sec5TestingGrounds/Terrain/Ground.cpp
@@ -18,47 +18,77 @@ void AGround::BeginPlay()
 void AGround::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	CastSphere(GetActorLocation(), 300);
-	CastSphere(GetActorLocation() + FVector(0, 0, 1000), 300);
 }
 
-void AGround::PlaceActors(TSubclassOf<AActor> ActorToSpawn, int32 MinSpawn, int32 MaxSpawn)
+/**
+* Places X Actors, where X is a random number between MinSpawn and MaxSpawn.
+* The Actors will be placed only if an empty location is found. The MaxAttempts parameter specifies the number of attempts to find an empty location.
+* The NeededSpaceRadius specifies the radius within which we will look for collisions. Must be the space needed by the Actor.
+*/
+void AGround::PlaceActors(TSubclassOf<AActor> ActorToSpawn, int32 MinSpawn, int32 MaxSpawn, int32 MaxAttempts, float NeededSpaceRadius)
 {
-	// Define the surface on which Actors have to Spawn. The surface is defined thanks to 2 points (Ground Floor Endpoints)
-	FVector BottomRightEndpoint = FVector(0, 1950, 100);
-	FVector TopLeftEndpoint = FVector(3950, -1950, 100);
-
 	int32 NumberToSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
 	for (int32 i = 0; i < NumberToSpawn; i++)
 	{
-		FVector SpawnPoint = FMath::RandPointInBox(FBox(BottomRightEndpoint, TopLeftEndpoint));
-		FRotator SpawnRotation = FRotator(0, FMath::FRandRange(0, 360), 0);
-		AActor* SpawnedActor = GetWorld()->SpawnActor<AActor>(ActorToSpawn, SpawnPoint, SpawnRotation);
-		SpawnedActor->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, true), NAME_None);
+		FVector SpawnPoint(0);
+		if (FindEmptyLocation(SpawnPoint, NeededSpaceRadius, MaxAttempts)) PlaceActor(ActorToSpawn, SpawnPoint);
 	}
 }
 
-bool AGround::CastSphere(FVector Location, float Radius)
+void AGround::PlaceActor(TSubclassOf<AActor> ActorToSpawn, FVector SpawnPoint)
+{
+	FRotator RandomSpawnRotation = FRotator(0, FMath::FRandRange(0, 360), 0);
+	AActor* SpawnedActor = GetWorld()->SpawnActor<AActor>(ActorToSpawn, SpawnPoint, RandomSpawnRotation);
+	SpawnedActor->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, true), NAME_None);
+}
+
+bool AGround::FindEmptyLocation(FVector& OutLocation, float NeededSpaceRadius, int32 MaxAttempts)
+{
+	// Define the surface on which Actors have to Spawn. The surface is defined thanks to 2 points (Ground Floor Endpoints). These points are relative to the Ground Actor.
+	FVector FloorBottomRightEndpoint = FVector(0, 1950, 100);
+	FVector FloorTopLeftEndpoint = FVector(3950, -1950, 100);
+	FBox SpawnableSurface = FBox(FloorBottomRightEndpoint, FloorTopLeftEndpoint);
+
+	for (int32 i = 0; i < MaxAttempts; i++)
+	{
+		FVector CandidateLocation = FMath::RandPointInBox(SpawnableSurface);
+		if (IsEmpty(CandidateLocation, NeededSpaceRadius))
+		{
+			OutLocation = CandidateLocation;
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool AGround::IsEmpty(FVector RelativeLocation, float Radius)
 {
 	FHitResult HitResult;
+	FVector GlobalLocation = GetActorLocation() + RelativeLocation;
+
+	/** Sphere Cast */
 	bool HasHit = GetWorld()->SweepSingleByChannel(
 		HitResult,
-		Location,
-		Location,
+		GlobalLocation,
+		GlobalLocation,
 		FQuat::Identity,
 		ECollisionChannel::ECC_GameTraceChannel2,
 		FCollisionShape::MakeSphere(Radius)
 	);
 
-	FLinearColor ResultColor = HasHit ? FLinearColor::Red : FLinearColor::Green;
-	UKismetSystemLibrary::DrawDebugSphere(
+	/** Draw Debug Cylinder useful to find the right Radius for an Actor */
+	/*FLinearColor ResultColor = HasHit ? FLinearColor::Red : FLinearColor::Green;
+	UKismetSystemLibrary::DrawDebugCylinder(
 		this,
-		Location,
+		GlobalLocation,
+		GlobalLocation + FVector(0, 0, 300),
 		Radius,
 		36,
-		ResultColor
-	);
+		ResultColor,
+		120
+	);*/
 
-	return HasHit;
+	/** The location is empty if any hit was found */
+	return !HasHit;
 }
--- Source/Sec5TestingGrounds/Terrain/Ground.h
+++ Source/Sec5TestingGrounds/Terrain/Ground.h
@@ -16,9 +16,17 @@ public:
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
 
+	/**
+	* Places X Actors, where X is a random number between MinSpawn and MaxSpawn.
+	* The Actors will be placed only if an empty location is found. The MaxAttempts parameter specifies the number of attempts to find an empty location.
+	* The NeededSpaceRadius specifies the radius within which we will look for collisions. Must be the space needed by the Actor.
+	*/
 	UFUNCTION(BlueprintCallable, Category = "Ground Generation")
-	void PlaceActors(TSubclassOf<AActor> ActorToSpawn, int32 MinSpawn, int32 MaxSpawn);
+	void PlaceActors(TSubclassOf<AActor> ActorToSpawn, int32 MinSpawn = 1, int32 MaxSpawn = 1, int32 MaxAttempts = 20, float NeededSpaceRadius = 500);
 
 private:
-	bool CastSphere(FVector Location, float Radius);
+	/** Utilities for PlaceActors */
+	void PlaceActor(TSubclassOf<AActor> ActorToSpawn, FVector SpawnPoint);
+	bool FindEmptyLocation(FVector& OutLocation, float NeededSpaceRadius, int32 MaxAttempts);
+	bool IsEmpty(FVector RelativeLocation, float Radius);
 };
